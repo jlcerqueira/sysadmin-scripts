32ba7c9c47c411a8b512be16ec0db80bc5b8c239
jlcerqueira <lcsgm@hotmail.com>
2025-08-14 09:39:13 -0300

    first commit

---
+++ powershell/ConsultaSNI.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module -Name WebAdministration
+
+Get-ChildItem -Path IIS:SSLBindings | ForEach-Object -Process `
+{
+if ($*.Sites)
+{
+$certificate = Get-ChildItem -Path CERT:LocalMachine/My |
+Where-Object -Property Thumbprint -EQ -Value $*.Thumbprint
+
+[PsCustomObject]@{
+Sites = $_.Sites.Value
+CertificateFriendlyName = $certificate.FriendlyName
+CertificateDnsNameList = $certificate.DnsNameList
+CertificateNotAfter = $certificate.NotAfter
+CertificateIssuer = $certificate.Issuer
+}
+}
+}
